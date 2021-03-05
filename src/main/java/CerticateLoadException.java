ad60bd716a7c0059cbef7f5d98541bb0f7cc1b27
Nils Tekampe <nt@konfdias.de>
2021-03-05 21:28:44 +0100

    Zwischenstand

--- src/main/java/CerticateLoadException.java
+++ src/main/java/CerticateLoadException.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CerticateLoadException {
+/**
+ * Diese Exception wird aus der Methode loadCertificate geworfen. Sie zeigt an,
+ * dass beim Laden des Zertifikats ein Fehler aufgetreten ist. Die Exception enthält
+ * eine message mit weitere Infos und eine innerException, die auf die Ursache des Fehlers zeigt
+ */
+
+public class CerticateLoadException extends Exception{
+
+    private String message;
+    private Exception innerExcpetion;
+
+    public CerticateLoadException(String message, Exception innerExcpetion){
+        this.message = message;
+        this.innerExcpetion = innerExcpetion;
+    }
+    public Exception getInnerExcpetion() {
+        return innerExcpetion;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
 }
--- src/main/java/TTC.java
+++ src/main/java/TTC.java
@@ -1,15 +1,16 @@
 import java.io.*;
+import java.nio.file.Path;
 import java.security.*;
 import java.security.cert.*;
 import java.util.*;
 import java.security.Security;
+import java.io.File;
+import java.nio.file.Files;
 
 import org.apache.commons.cli.*;
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.bouncycastle.asn1.*;
-import org.bouncycastle.jcajce.provider.asymmetric.X509;
 import org.bouncycastle.operator.AlgorithmNameFinder;
 import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.slf4j.Logger;
@@ -28,7 +29,6 @@ public class TTC {
     public static void main(String[] args) throws IOException {
         Security.addProvider(new BouncyCastleProvider());
 
-        options.addOption("h", "Hilfe", false, "zeige die Hilfe.");
         options.addOption("i", "InputTAR", true, "Das TAR Archiv, das geprüft werden soll.");
         options.addOption("t", "TrustAnker", true, "Trust Anker in Form eines X.509 Zertifikats für die Root-CA");
         options.addOption("o", "OverwriteCertCheck", false, "Wenn diese Option gesetzt wird, werden die Zertifikate im TAR Archiv nicht gegen eine Root-CA geprüft");
@@ -39,6 +39,9 @@ public class TTC {
         String trustCertPath = "";
         X509Certificate trustedCert = null;
 
+        /*********************************
+         ** Argumente des Aufrufs prüfen *
+         *********************************/
         try {
             cmd = parser.parse(options, args);
             if (cmd.hasOption("i") == false) {
@@ -50,30 +53,29 @@ public class TTC {
 
             if (cmd.hasOption("t")) {
                 trustCertPath = cmd.getOptionValue("t");
+                try {
+                    byte[] certFileContent = Files.readAllBytes(Path.of(trustCertPath));
+                    trustedCert = loadCertificate(certFileContent);
+                }
+                catch (CerticateLoadException | IOException e) {
+                    e.printStackTrace();
+                }
             }
 
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             logger.error("Fehler beim parsen der Kommandozeile. " + e.getLocalizedMessage());
         }
 
-//        Sofern wir das Zertifikat für die RootCA brauchen, laden wir es jetzt
-        if (cmd.hasOption("o") == false) {
-
-                // FIXME: Call verification function
-
-            }
-
-        /* Wir lesen nun einmal durch das TAR Archiv (ohne es zu entpacken)*/
+        /********************************************************************
+         ** Wir lesen nun einmal durch das TAR Archiv (ohne es zu entpacken)*
+         ********************************************************************/
         try {
             TarArchiveInputStream myTarFile = new TarArchiveInputStream(new FileInputStream(new File(cmd.getOptionValue("i"))));
-
-            /* To read individual TAR file */
             TarArchiveEntry entry = null;
             String individualFileName;
-
             int offset;
-            FileOutputStream outputFile = null;
-            /* Create a loop to read every single entry in TAR file */
+
             while ((entry = myTarFile.getNextTarEntry()) != null) {
                 /* Get the name of the file */
                 individualFileName = entry.getName();
@@ -84,96 +86,144 @@ public class TTC {
 
                 myTarFile.read(content, offset, content.length - offset);
 
+                /***************************
+                 ** TransactionLog Message *
+                 ***************************/
                 if (individualFileName.matches("^(Gent_|Unixt_|Utc_).+_Sig-\\d+_Log-.+(Start|Update|Finish)_Client-.+log")) {
                     logger.info("{} seems to be a transaction log. Will process it now.", individualFileName);
                     TransactionLogMessage log = new TransactionLogMessage(content, individualFileName);
                     all_log_messages.add(log);
-
-                } else if (individualFileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys.+log")) {
+                }
+                /**********************
+                 ** SystemLog Message *
+                 **********************/
+                else if (individualFileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys.+log")) {
                     logger.info("{} seems to be a system log. Will process it now.", individualFileName);
                     SystemLogMessage log = new SystemLogMessage(content, individualFileName);
                     all_log_messages.add(log);
 
-                } else if (individualFileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Aud.+log")) {
+                }
+                /**********************
+                 ** AuditLog Message *
+                 *********************/
+                else if (individualFileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Aud.+log")) {
                     logger.info("{} seems to be a audit log. Will process it now.", individualFileName);
                     AuditLogMessage log = new AuditLogMessage(content, individualFileName);
                     all_log_messages.add(log);
 
-                } else if (individualFileName.contains("CVC")) {
+                }
+                /*********************
+                 ** CVC Certificate *
+                 ********************/
+                else if (individualFileName.contains("CVC")) {
                     logger.info("{} seems to be a CVC. Will process it now.", individualFileName);
 
-                } else if (individualFileName.contains("X509")) {
+                }
+                /**********************
+                 ** X.509 Certificate *
+                 **********************/
+                else if (individualFileName.contains("X509")) {
                     logger.info("{} seems to be an X509 certificate. Will process it now.", individualFileName);
 
                     try {
-                        CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
-                        InputStream in = new ByteArrayInputStream(content);
-                        X509Certificate cer = (X509Certificate) cf.generateCertificate(in);
-
-                        //Prüfen, dass das Zertifikat gültig ist
-                        cer.checkValidity();
-
-                        //Als nächstes prüfen wir, ob das Zertifikat die keyUsage für crlSign hat. In dem Fall behandeln wir es als intermedite Zertifikat, ansonsten als Client-Zert
+                        X509Certificate cer = loadCertificate(content);
                         boolean[] keyUsage = cer.getKeyUsage();
                         if (keyUsage == null || keyUsage[5] == false) {
                             allClientCertificates.put(individualFileName.split("_")[0].toUpperCase(), cer);
-                        } else {
+                        }
+                        else {
                             allIntermediateCertificates.put(individualFileName.split("_")[0].toUpperCase(), cer);
                         }
-
-                    } catch (CertificateExpiredException e) {
-                        logger.error("{} contains an expired certificate", individualFileName);
-                    } catch (CertificateNotYetValidException e) {
-                        logger.error("{} contains a certificate that is not yet valid", individualFileName);
-                    } catch (java.security.cert.CertificateException e) {
-                        throw new RuntimeException(e);
-                    } catch (NoSuchProviderException e) {
-                        e.printStackTrace();
                     }
-                } else {
+                    catch (CerticateLoadException e) {
+                        logger.error("Fehler beim Laden des Zertifikats {}", individualFileName);
+                    }
+
+                }
+                else {
                     logger.error("{} sollte nicht in der TAR Datei vorhanden sein. Es wird ignoriert.", individualFileName);
                 }
             }
 
-            // Nun geben wir alle Nachrichten einmal aus.
-            for (int i = 0; i < all_log_messages.size(); i++) {
-                System.out.println(all_log_messages.get(i).prettyPrint());
+            /***********************************************
+             ** Nun geben wir alle Nachrichten einmal aus.*
+             **********************************************/
+            for (LogMessage message : all_log_messages) {
+                System.out.println(message.prettyPrint());
             }
 
-
-            //Sofern nicht darauf verzichtet wid, prüfen wir die Gültikeit der Client-Zertifikate
+            /***************************************************************************************
+             ** Sofern nicht darauf verzichtet wid, prüfen wir die Gültikeit der Client-Zertifikate*
+             ***************************************************************************************/
             if (cmd.hasOption("o") == false) {
                 for (X509Certificate cert : allClientCertificates.values()) {
                     try {
-                        Boolean result = checkCert(cert, trustStore, new ArrayList<X509Certificate>(allIntermediateCertificates.values()));
+
+                        Boolean result = checkCert(cert, trustedCert, new ArrayList<X509Certificate>(allIntermediateCertificates.values()));
                         logger.info("Prüfe das Zertifikat mit Seriennummer {} auf Korrektheit und prüfe die zugehörige Zertifikatskette. Ergebnis ist {}", cert.getSerialNumber(), result.toString());
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
             }
 
-            // Im nächsten Schritt prüfen wir alle Signaturen der Log-Messages einmal
-            for (int i = 0; i < all_log_messages.size(); i++) {
+            /***************************************************************************
+             ** Im nächsten Schritt prüfen wir alle Signaturen der Log-Messages einmal *
+             ***************************************************************************/
+            for (LogMessage message : all_log_messages) {
                 try {
-                    assertSignatureOfLogMessageIsValid(all_log_messages.get(i), allClientCertificates.get(all_log_messages.get(i).serialNumber));
-                    logger.info("Prüfe die Signatur der LogMessage {}", all_log_messages.get(i).filename);
-                } catch (SignatureValidationException signatureValidationException) {
-                    logger.error("Fehler bei der Prüfung der Signatur");
+                    assertSignatureOfLogMessageIsValid(message, allClientCertificates.get(message.serialNumber));
+                    logger.info("Prüfe die Signatur der LogMessage {}", message.filename);
+                }
+                catch (SignatureValidationException signatureValidationException) {
+                    logger.error("Fehler bei der Prüfung der Signatur der Log Message {}", message.filename);
                 }
             }
             myTarFile.close();
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
 
 
-    public static X509Certificate loadCertificate(byte[] certContent){
+    /**
+     * Diese Funktion lädt ein X059Certificat aus einem ByteArray und kümmert sich um die Fehlerbehandlung
+     *
+     * @param certContent Ein Byte-Array, das das Zertifikat enthält
+     * @return das X509Certificate Objekt
+     * @throws CerticateLoadException
+     */
+    public static X509Certificate loadCertificate(byte[] certContent) throws CerticateLoadException {
+        X509Certificate cer = null;
+        try {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
+            InputStream in = new ByteArrayInputStream(certContent);
+            cer = (X509Certificate) cf.generateCertificate(in);
 
+            /*******************************************
+             ** Prüfen, dass das Zertifikat gültig ist *
+             *******************************************/
+            cer.checkValidity();
 
+        }
+        catch (CertificateExpiredException e) {
+            logger.error("The byte buffer contains an expired certificate");
+        }
+        catch (CertificateNotYetValidException e) {
+            logger.error("The byte buffer contains a certificate that is not yet valid");
+        }
+        catch (java.security.cert.CertificateException e) {
+            throw new RuntimeException(e);
+        }
+        catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return cer;
     }
+
     /**
      * @param certToCheck       Das Zertifikat, dessen Gültigkeit geprüft ewrden soll
      * @param trustStore        Ein TrustStore, der das Root-Zertifikat enthält
@@ -186,7 +236,9 @@ public class TTC {
      * @throws CertPathValidatorException
      * @throws CertificateException
      */
-    public static boolean checkCert(X509Certificate certToCheck, X509Certificate trustedCert, List<X509Certificate> intermediateCerts) throws NoSuchAlgorithmException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchProviderException, CertPathValidatorException, CertificateException {
+    public static boolean checkCert(X509Certificate certToCheck, X509Certificate
+            trustedCert, List<X509Certificate> intermediateCerts) throws
+            NoSuchAlgorithmException, KeyStoreException, InvalidAlgorithmParameterException, NoSuchProviderException, CertPathValidatorException, CertificateException {
 //FIXME: Noch nicht implementiert
         //        CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
 //        CertPath path = cf.generateCertPath(intermediateCerts);
@@ -212,7 +264,8 @@ public class TTC {
      * @param certForVerification Das Zertifikat, das zur Prüfung der LogMessage verwendet werden soll
      * @throws SignatureValidationException Dieser Fehler wird geworfen fall die Validierung der Signatur fehlschlägt. Im Innern der Exception findet sich die ursprüngliche Exception und eine Fehlernachricht
      */
-    public static void assertSignatureOfLogMessageIsValid(LogMessage logMessageToVerify, X509Certificate certForVerification) throws SignatureValidationException {
+    public static void assertSignatureOfLogMessageIsValid(LogMessage logMessageToVerify, X509Certificate
+            certForVerification) throws SignatureValidationException {
 
         if (certForVerification == null) {
             throw new SignatureValidationException("Es wurde kein gültiges Zertifikat für die Prüfung der Signatur übergeben", new NullPointerException());
@@ -231,16 +284,19 @@ public class TTC {
             st.update(logMessageToVerify.dtbs);
 
             byte[] signatureValue = logMessageToVerify.signatureValue;
-            String testString = Hex.encodeHexString(signatureValue);
-            Boolean resultSignatureVerification = st.verify(signatureValue);
+            st.verify(signatureValue);
 
-        } catch (NoSuchProviderException e) {
+        }
+        catch (NoSuchProviderException e) {
             throw new SignatureValidationException("Bouncy Castle wurde als Provider nicht gefunden", e);
-        } catch (NoSuchAlgorithmException e) {
+        }
+        catch (NoSuchAlgorithmException e) {
             throw new SignatureValidationException("Der Algorithmus wird nicht unterstützt", e);
-        } catch (SignatureException e) {
+        }
+        catch (SignatureException e) {
             throw new SignatureValidationException("Die Signatur konnte nicht verifiziert werden.", e);
-        } catch (InvalidKeyException e) {
+        }
+        catch (InvalidKeyException e) {
             logger.error("Der Schlüssel zur Prüfung der Signatur konnte nicht eingelesen werden.", e);
         }
 
