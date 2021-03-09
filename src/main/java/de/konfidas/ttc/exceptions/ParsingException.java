d8225b30be7e776d2a367b09f9222f755fa97f7d
Daniel Heldt <dheldt@users.noreply.github.com>
2021-03-09 08:03:46 +0100

    Working on OIDs, creating general ParsingException

--- src/main/java/de/konfidas/ttc/exceptions/ParsingException.java
+++ src/main/java/de/konfidas/ttc/exceptions/ParsingException.java
@@ -1,2 +1,10 @@
-package de.konfidas.ttc.exceptions;public class ParsingException {
+package de.konfidas.ttc.exceptions;
+
+public class ParsingException extends TtcException{
+    public ParsingException(String message, Exception reason) {
+        super(message, reason);
+    }
+    public ParsingException(String message) {
+        super(message, null);
+    }
 }
--- src/main/java/de/konfidas/ttc/utilities/OID.java
+++ src/main/java/de/konfidas/ttc/utilities/OID.java
@@ -1,24 +1,50 @@
 package de.konfidas.ttc.utilities;
 
+import de.konfidas.ttc.exceptions.ParsingException;
 import org.bouncycastle.util.encoders.Hex;
 
-public enum OIDs {
+import java.util.Arrays;
 
-    id_SE_API_transaction_log(Hex.decode(""), "0.4.0.127.0.7.3.7.1.1", "id-SE-API-transaction-log"),
-    id_SE_API_SE_audit_log(Hex.decode(""), "0.4.0.127.0.7.3.7.1.1", "id-SE-API-SE-audit-log"),
-    id_SE_API_system_log(Hex.decode(""), "0.4.0.127.0.7.3.7.1.1", "id-SE-API-system-log");
+public enum OID {
+
+    id_SE_API_transaction_log( Hex.decode("06 09 04 00 7F 00 07 03 07 01 01".replace("\\\\s+","")), "0.4.0.127.0.7.3.7.1.1", "id-SE-API-transaction-log"),
+    id_SE_API_system_log     ( Hex.decode("06 09 04 00 7F 00 07 03 07 01 02".replace("\\\\s+","")), "0.4.0.127.0.7.3.7.1.2", "id-SE-API-system-log"),
+    id_SE_API_SE_audit_log   ( Hex.decode("06 09 04 00 7F 00 07 03 07 01 03".replace("\\\\s+","")), "0.4.0.127.0.7.3.7.1.3", "id-SE-API-SE-audit-log");
 
     byte[] encoded;
     String readable;
     String name;
 
-    OIDs(byte[] encoded, String readable, String name){
+    OID(byte[] encoded, String readable, String name){
         this.encoded = encoded;
         this.readable = readable;
         this.name = name;
     }
 
-    public static OIDs fromBytes( byte[] encoded){
+    public byte[] getEncoded(){
+        return encoded;
+    }
 
+    public String getName(){
+        return name;
+    }
+
+    public String getReadable() {
+        return readable;
+    }
+
+    public static OID fromBytes(byte[] encoded) throws UnknownOidException {
+        for(OID oid : OID.values()){
+            if (Arrays.equals(oid.getEncoded(), encoded)){
+                return oid;
+            }
+        }
+        throw new UnknownOidException("unknown Oid:"+Hex.toHexString(encoded));
+    }
+
+    public static class UnknownOidException extends ParsingException {
+        public UnknownOidException(String message) {
+            super(message);
+        }
     }
 }
