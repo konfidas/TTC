3617422b33acc9ecf4ca0e094d3f0ef35a0b21d2
Nils Tekampe <nt@konfdias.de>
2021-03-13 17:33:06 +0100

    Nachtrag

--- src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestInconsinstentCertificate.java
+++ src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestInconsinstentCertificate.java
@@ -1,2 +1,69 @@
-package de.konfidas.ttc.tars;public class LogMessageArchiveTestInconsinstentCertificate {
+
+package de.konfidas.ttc.tars;
+
+import de.konfidas.ttc.exceptions.BadFormatForTARException;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.fail;
+
+@RunWith(Parameterized.class)
+public class LogMessageArchiveTestInconsinstentCertificate {
+    final static Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+    final static File brokenTarFiles = new File("testdata/negative/inconsistent_certificates/");
+
+    File file;
+
+    @Before
+    public void initialize() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection filesToTest() {
+
+        logger.info("checking for Tars in " + brokenTarFiles.getName());
+        if (null == brokenTarFiles || !brokenTarFiles.isDirectory()) {
+            fail("not a directory.");
+
+            return Collections.EMPTY_LIST;
+        }
+
+        return Arrays.asList(brokenTarFiles.listFiles());
+    }
+
+    public LogMessageArchiveTestInconsinstentCertificate(File file) {
+        this.file = file;
+    }
+
+    @Test
+    public void parse() throws IOException {
+        logger.info("");
+        logger.info("============================================================================");
+        logger.info("testing tar file {}:", file.getName());
+
+        try {
+            LogMessageArchive tar = new LogMessageArchive(this.file);
+
+            fail("Log Message parsing successful, but expected to fail");
+        }
+        catch (IOException | BadFormatForTARException e) {
+            // expected behaviour
+
+        }
+
+    }
 }
