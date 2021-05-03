5205a9d6e2e073450de4f5b3143d834bb2eb8819
Daniel Heldt <dheldt@users.noreply.github.com>
2021-05-03 08:21:37 +0200

    Minor improvement of LogMessageArchive, allowing to overload return types

--- src/main/java/de/konfidas/ttc/tars/LogMessageArchive.java
+++ src/main/java/de/konfidas/ttc/tars/LogMessageArchive.java
@@ -7,9 +7,9 @@ import java.util.Collection;
 import java.util.Map;
 
 public interface LogMessageArchive {
-    Map<String, X509Certificate> getIntermediateCertificates();
-    Map<String, X509Certificate> getClientCertificates();
-    Collection<LogMessage> getLogMessages();
+    Map<? extends String, ? extends X509Certificate> getIntermediateCertificates();
+    Map<? extends String, ? extends X509Certificate> getClientCertificates();
+    Collection<? extends LogMessage> getLogMessages();
 
     /**
      * A lazy getter, returning the Collection of LogMessages in a sorted manner. The messages are sorted
@@ -18,5 +18,5 @@ public interface LogMessageArchive {
      * times for different serial numbers.
      * @return a collection of all Log Messages of the Archive, sorted by increasing Signature Counter.
      */
-    Collection<LogMessage> getSortedLogMessages();
+    Collection<? extends LogMessage> getSortedLogMessages();
 }
