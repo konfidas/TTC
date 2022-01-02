b4680083e392155827d69c24656aa4d42b3559a9
Nils Tekampe <nt@konfidas.de>
2022-01-02 13:24:06 +0100

    Tuned regex expression in LogMessageFactory, removed corrupted test data

--- src/main/java/de/konfidas/ttc/messages/LogMessageFactory.java
+++ src/main/java/de/konfidas/ttc/messages/LogMessageFactory.java
@@ -23,98 +23,101 @@ public class LogMessageFactory {
 
     public static LogMessageImplementation createLogMessage(String fileName, byte[] content) throws BadFormatForLogMessageException {
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_).+_Sig-\\d+_Log-.+(Start|Update|Finish)_Client-.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_).+_Sig-\\d+_Log-.+(Start|Update|Finish)_Client-.+log$")) {
             logger.debug("{} seems to be a TransactionLog. Processing it now. ", fileName);//NON-NLS
            return new TransactionLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_unblockUser.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_unblockUser.+log$")) {
             logger.debug("{} seems to be an unblockUser systemLog. Processing it now. ", fileName);//NON-NLS
             return new UnblockUserSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_authenticateUser.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_authenticateUser.+log$")) {
             logger.debug("{} seems to be an authenitcateUser systemLog. Processing it now. ", fileName);//NON-NLS
             return new AuthenticateUserSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_authenticateSmaersAdmin.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_authenticateSmaersAdmin.+log$")) {
             logger.debug("{} seems to be an authenticateSmaersAdmin systemLog. Processing it now. ", fileName);//NON-NLS
             return new AuthenticateSmaersAdminSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_registerClient.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_registerClient.+log$")) {
             logger.debug("{} seems to be a registerClient systemLog. Processing it now.", fileName);//NON-NLS
             return new RegisterClientLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_deregisterClient.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_deregisterClient.+log$")) {
             logger.debug("{} seems to be a deregisterClient systemLog. Processing it now.", fileName);//NON-NLS
             return new DeregisterClientLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_startAudit.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_startAudit.+log$")) {
             logger.debug("{} seems to be a startAudit systemLog. Processing it now.", fileName);//NON-NLS
             return new StartAuditSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_initialize.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_initialize.+log$")) {
             logger.debug("{} seems to be a initialize systemLog. Processing it now.", fileName);//NON-NLS
             return new InitializeSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_updateTime.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_updateTime.+log$")) {
             logger.debug("{} seems to be an upateTime log. Processing it now.", fileName);//NON-NLS
             return new UpdateTimeSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_logOut.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_logOut.+log$")) {
             logger.debug("{} seems to be an Logut System log. Processing it now.", fileName);//NON-NLS
             return new LogoutSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_enterSecureState.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_enterSecureState.+log$")) {
             logger.debug("{} seems to be an enterSecureState System log. Processing it now.", fileName);//NON-NLS
             return new EnterSecureStateSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_configureLogging.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_configureLogging.+log$")) {
             logger.debug("{} seems to be an configureLogging System log. Processing it now.", fileName);//NON-NLS
             return new ConfigureLoggingSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_unlockTransactionLogging.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_unlockTransactionLogging.+log$")) {
             logger.debug("{} seems to be an unlockTransactionLogging System log. Processing it now.", fileName);//NON-NLS
             return new UnlockTransactionLoggingLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_lockTransactionLogging.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_lockTransactionLogging.+log$")) {
             logger.debug("{} seems to be an lockTransactionLogging System log. Processing it now.", fileName);//NON-NLS
             return new LockTransactionLoggingLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_selfTest.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_selfTest.+log$")) {
             logger.debug("{} seems to be an selfTest System log. Processing it now.", fileName);//NON-NLS
             return new SelfTestSystemLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_deleteStoredData.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_deleteStoredData.+log$")) {
             logger.debug("{} seems to be an deleteStoredData System log. Processing it now.", fileName);//NON-NLS
             return new DeleteStoredDataLogMessage(content, fileName);
         }
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_exitSecureState.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_disableSecureElement.+log$")) {
+            logger.debug("{} seems to be a disableSecureElement System log. Processing it now.", fileName);//NON-NLS
+            return new DisableSecureElementSystemLogMessage(content, fileName);
+        }
+
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Sys_exitSecureState.+log$")) {
             logger.debug("{} seems to be an exitSecureState System log. Processing it now.", fileName);//NON-NLS
             return new ExitSecureStateLogMessage(content, fileName);
         }
 
 
-        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Aud.+log")) {
+        if (fileName.matches("^(Gent_|Unixt_|Utc_)\\d+_Sig-\\d+_Log-Aud.+log$")) {
             logger.debug("{} seems to be an auditLog. Processing it now.", fileName);//NON-NLS
             return new AuditLogMessage(content, fileName);
         }
-
-
 
         throw new BadFormatForLogMessageException(String.format(properties.getString("de.konfidas.ttc.messages.fileNameUnknownTypeOfLogMessages"),fileName));
 
