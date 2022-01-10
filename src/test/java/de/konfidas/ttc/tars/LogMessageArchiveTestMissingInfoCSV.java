3c8435645cd41cc73a19429f07cb9ad30f077896
Frank Schlesinger <Frank.Schlesinger@orderbird.com>
2022-01-10 19:04:15 +0100

    Refactor LogMessageArchiveTestMissingInfoCSV and remove redundant test class

--- src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestMissingInfoCSV.java
+++ src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestMissingInfoCSV.java
@@ -1,65 +1,31 @@
-
 package de.konfidas.ttc.tars;
 
 import de.konfidas.ttc.exceptions.BadFormatForTARException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.security.Security;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-@RunWith(Parameterized.class)
 public class LogMessageArchiveTestMissingInfoCSV {
-    final static Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-    final static File brokenTarFiles = new File("testdata/negative/testdata_no_info_csv/");
 
-    final File file;
-
-    @Before
+    @BeforeEach
     public void initialize() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-
-    @Parameterized.Parameters
-    public static Collection<File> filesToTest() {
-
-        logger.debug("checking for Tars in " + brokenTarFiles.getName());
-        if (!brokenTarFiles.isDirectory() || brokenTarFiles.listFiles() == null) {
-            return Collections.EMPTY_LIST;
-        }
-
-        return Arrays.asList(brokenTarFiles.listFiles());
-    }
-
-    public LogMessageArchiveTestMissingInfoCSV(File file) {
-        this.file = file;
-    }
-
     @Test
-    public void parse() throws IOException {
-        logger.debug("");
-        logger.debug("============================================================================");
-        logger.debug("testing tar file {}:", file.getName());
+    public void parseTARWithMissingInfoCSV_shouldThrowException() throws Exception {
 
         try {
-            new LogMessageArchiveImplementation(this.file);
-            fail("Log Message parsing successful, but expected to fail");
+            new LogMessageArchiveImplementation(new File(File.separator + "testdata" + File.separator + "negative" + File.separator + "testdata_no_info_csv" + File.separator + "testdata_no_info_csv.tar"));
+            fail("The expected Exception was not thrown when parsing a broken TAR file.");
+        } catch (BadFormatForTARException e) {
+            assertTrue(true);
         }
-        catch (BadFormatForTARException e) {
-            // expected behaviour
-        }
-
     }
 }
