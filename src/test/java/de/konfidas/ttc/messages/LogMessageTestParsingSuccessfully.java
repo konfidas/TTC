0cc2fa5f077f9ce9520f3aba72c62fd2d6bd9479
Nils Tekampe <nt@konfidas.de>
2022-01-10 22:45:16 +0100

    Fixed LogMessageTestParsingSuccessfully but one log message still failing

--- src/test/java/de/konfidas/ttc/messages/LogMessageTestParsingSuccessfully.java
+++ src/test/java/de/konfidas/ttc/messages/LogMessageTestParsingSuccessfully.java
@@ -2,60 +2,46 @@ package de.konfidas.ttc.messages;
 
 import de.konfidas.ttc.exceptions.BadFormatForLogMessageException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.Security;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
-@RunWith(Parameterized.class)
 public class LogMessageTestParsingSuccessfully {
     final static Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
     final static File correctLogs = new File("./testData/logMessages"); // TODO: as soon as we have publish-able test data, point path to it.
 
-    final File file;
-
-    @Before
+    @BeforeEach
     public void initialize() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
 
-    @Parameterized.Parameters
-    public static Collection<File> filesToTest(){
 
-        logger.debug("Checking for Logs in "+correctLogs.getName());
-        if(!correctLogs.isDirectory() || correctLogs.listFiles() == null){
-            logger.error("not a directory.");
-            return Collections.EMPTY_LIST;
-        }
-
-        return Arrays.asList(correctLogs.listFiles());
-    }
-
-
-    public LogMessageTestParsingSuccessfully(File file){
-        this.file = file;
-    }
-
-    @Ignore
-    @Test
-    public void parse() throws IOException, BadFormatForLogMessageException {
+    @ParameterizedTest(name = "LogMessageTestParsingSuccessfully. Rest {index} => file={0}")
+    @MethodSource("filesToTest")
+    public void parse(File file) throws IOException, BadFormatForLogMessageException {
         logger.debug("");
         logger.debug("============================================================================");
         logger.debug("parsing log message {}:", file.getName());
 
-        LogMessageFactory.createLogMessage(this.file);
+        LogMessageFactory.createLogMessage(file);
 
+    }
+
+    public static Stream<Arguments> filesToTest(){
+        return Stream.of(
+                Arguments.of(new File("testdata/logMessages/Unixt_1607348284_Sig-2_Log-Aud.log")),
+                Arguments.of(new File("testdata/logMessages/Unixt_1607363188_Sig-11_Log-Sys_unblockUser.log")),
+                Arguments.of(new File("testdata/logMessages/Unixt_1607363188_Sig-13_Log-Sys_setConfiguration.log")));
     }
 }
