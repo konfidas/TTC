f34c807406463dc14449cab69ccd0d547dcb69f6
Nils Tekampe <nt@konfidas.de>
2022-01-10 22:52:05 +0100

    Fixing LogMessageArchiveTestInconsistentCertificate

--- src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestInconsistentCertificate.java
+++ src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestInconsistentCertificate.java
@@ -3,11 +3,6 @@ package de.konfidas.ttc.tars;
 
 import de.konfidas.ttc.exceptions.BadFormatForTARException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,47 +13,50 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-import static org.junit.Assert.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
 
-@RunWith(Parameterized.class)
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 public class LogMessageArchiveTestInconsistentCertificate {
     final static Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
     final static File brokenTarFiles = new File("testdata/negative/inconsistent_certificates/");
 
-    final File file;
 
-    @Before
+    @BeforeEach
     public void initialize() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
 
-    @Parameterized.Parameters
-    public static Collection<File> filesToTest() {
-        logger.debug("checking for Tars in " + brokenTarFiles.getName());
-        if (!brokenTarFiles.isDirectory() || brokenTarFiles.listFiles() == null) {
-            return Collections.EMPTY_LIST;
-        }
-        return Arrays.asList(brokenTarFiles.listFiles());
-    }
-
-    public LogMessageArchiveTestInconsistentCertificate(File file) {
-        this.file = file;
-    }
-
-    @Ignore
-    @Test
-    public void parse() throws IOException {
+    @ParameterizedTest(name = "LogMessageArchiveTestInconsistentCertificate. Rest {index} => file={0}")
+    @MethodSource("filesToTest")
+    public void parse(File file ) throws IOException {
         logger.debug("");
         logger.debug("============================================================================");
         logger.debug("testing tar file {}:", file.getName());
 
         try {
-            new LogMessageArchiveImplementation(this.file);
+            new LogMessageArchiveImplementation(file);
             fail("Log Message parsing successful, but expected to fail");
         }
         catch ( BadFormatForTARException e) {
             // expected behaviour
         }
     }
+    public static Stream<Arguments> filesToTest(){
+
+        logger.debug("checking for Tars in " + brokenTarFiles.getName());
+        if (!brokenTarFiles.isDirectory() || brokenTarFiles.listFiles() == null) {
+            return Stream.of();
+        }
+        return Stream.of(
+                Arguments.of(brokenTarFiles.listFiles()));
+    }
+
+
 }
