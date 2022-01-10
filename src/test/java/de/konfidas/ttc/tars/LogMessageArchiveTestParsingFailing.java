1a6021661ed6f03be472a08de9541b00cb699ee4
Frank Schlesinger <Frank.Schlesinger@orderbird.com>
2022-01-10 18:00:18 +0100

    Refactor and move LogMessageArchiveTestParsingFailing to Jupiter

--- src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestParsingFailing.java
+++ src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestParsingFailing.java
@@ -1,67 +1,32 @@
 package de.konfidas.ttc.tars;
 
-import de.konfidas.ttc.validation.AggregatedValidator;
-import de.konfidas.ttc.validation.CertificateFileNameValidator;
-import de.konfidas.ttc.validation.LogMessageSignatureValidator;
-import de.konfidas.ttc.validation.Validator;
+import de.konfidas.ttc.exceptions.BadFormatForTARException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.security.Security;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-@RunWith(Parameterized.class)
+
 public class LogMessageArchiveTestParsingFailing {
-    final static Logger logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-    final static File brokenTarFiles = new File("D:\\testdata\\brokenTars"); // TODO: as soon as we have publish-able test data, point path to it.
 
-    final File file;
-
-    @Before
+    @BeforeEach
     public void initialize() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
-
-    @Parameterized.Parameters
-    public static Collection<File> filesToTest(){
-
-        logger.debug("checking for Tars in "+brokenTarFiles.getName());
-        if(!brokenTarFiles.isDirectory() ||brokenTarFiles.listFiles() == null){
-            logger.error("not a directory.");
-            return Collections.EMPTY_LIST;
-        }
-
-        return Arrays.asList(brokenTarFiles.listFiles());
-    }
-
-    public LogMessageArchiveTestParsingFailing(File file){
-        this.file = file;
-    }
-
     @Test
-    public void parse() throws Exception{
-        logger.debug("");
-        logger.debug("============================================================================");
-        logger.debug("testing tar file {}:", file.getName());
+    public void parseBrokenTAR_shouldThrowException() throws Exception {
 
-        LogMessageArchiveImplementation tar = new LogMessageArchiveImplementation(this.file);
-
-        Validator v = new AggregatedValidator()
-                    .add(new CertificateFileNameValidator())
-                    .add(new LogMessageSignatureValidator());
-
-        assertFalse(v.validate(tar).getValidationErrors().isEmpty());
-
+        try {
+            new LogMessageArchiveImplementation(new File("parsingFailing.tar"));
+            fail("The expected Exception was not thrown when parsing a broken TAR file.");
+        } catch (BadFormatForTARException e) {
+            assertTrue(true);
+        }
     }
 }
