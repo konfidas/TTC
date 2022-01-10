f413de2923fbe7240702183100bcf40e43bdc362
Frank Schlesinger <Frank.Schlesinger@orderbird.com>
2022-01-10 18:56:12 +0100

    Refactor PropertyFiles to Jupiter

--- src/test/java/de/konfidas/ttc/setup/PropertyFiles.java
+++ src/test/java/de/konfidas/ttc/setup/PropertyFiles.java
@@ -1,6 +1,7 @@
 package de.konfidas.ttc.setup;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -8,15 +9,18 @@ import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 
-public  class PropertyFiles {
+public class PropertyFiles {
 
     @Test
-    public   void testContentOfPropertyFilesIsExistingDE() throws IOException{
-    // List Of Properties can be created by
-    // grep -RE "properties.getString.*" . | grep -oE "de\.konfidas.*\"" | cut -d '=' -f 2 | sed 's/\"$//' > listOfProperties.txt
+    public void testContentOfPropertyFilesIsExistingDE() throws IOException {
+        // List Of Properties can be created by
+        // grep -RE "properties.getString.*" . | grep -oE "de\.konfidas.*\"" | cut -d '=' -f 2 | sed 's/\"$//' > listOfProperties.txt
 
         File file = new File(System.getProperty("user.dir") + "/src/main/resources/");
         URL[] urls = {file.toURI().toURL()};
@@ -26,18 +30,22 @@ public  class PropertyFiles {
         ResourceBundle properties = ResourceBundle.getBundle("ttc", locale, loader);
 
         List<String> listOfProperties = Collections.emptyList();
-        String inputFile= System.getProperty("user.dir") + "/testdata/"+"listOfProperties.txt";
-        try { listOfProperties = Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8); } catch (IOException e) { throw new IOException(); }
+        String inputFile = System.getProperty("user.dir") + "/testdata/" + "listOfProperties.txt";
+        try {
+            listOfProperties = Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IOException();
+        }
 
-            for (String property: listOfProperties){
-                String a = properties.getString(property);
-                int aaa=0;
-            }
+        for (String property : listOfProperties) {
+            String a = properties.getString(property);
+            int aaa = 0;
+        }
 
     }
 
     @Test
-    public   void testContentOfPropertyFilesIsExistingEN() throws IOException{
+    public void testContentOfPropertyFilesIsExistingEN() throws IOException {
         // List Of Properties can be created by
         // grep -RE "properties.getString.*" . | grep -oE "de\.konfidas.*\"" | cut -d '=' -f 2 | sed 's/\"$//' > listOfProperties.txt
 
@@ -49,10 +57,14 @@ public  class PropertyFiles {
         ResourceBundle properties = ResourceBundle.getBundle("ttc", locale, loader);
 
         List<String> listOfProperties = Collections.emptyList();
-        String inputFile= System.getProperty("user.dir") + "/testdata/"+"listOfProperties.txt";
-        try { listOfProperties = Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8); } catch (IOException e) { throw new IOException(); }
+        String inputFile = System.getProperty("user.dir") + "/testdata/" + "listOfProperties.txt";
+        try {
+            listOfProperties = Files.readAllLines(Paths.get(inputFile), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IOException();
+        }
 
-        for (String property: listOfProperties){
+        for (String property : listOfProperties) {
             properties.getString(property);
         }
 
