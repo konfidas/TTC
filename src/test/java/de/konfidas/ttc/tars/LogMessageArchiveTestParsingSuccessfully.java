9d15670703995174d804c4e872b2cadc969c65a9
Nils Tekampe <nt@konfidas.de>
2022-01-10 22:37:23 +0100

    Fixing LogMessageArchiveTestParsingSuccessfully

--- src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestParsingSuccessfully.java
+++ src/test/java/de/konfidas/ttc/tars/LogMessageArchiveTestParsingSuccessfully.java
@@ -7,62 +7,51 @@ import de.konfidas.ttc.validation.CertificateFileNameValidator;
 import de.konfidas.ttc.validation.LogMessageSignatureValidator;
 import de.konfidas.ttc.validation.Validator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import java.util.stream.Stream;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.Security;
-import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(Parameterized.class)
+
 public class LogMessageArchiveTestParsingSuccessfully {
     final static Logger logger = LoggerFactory.getLogger(LogMessageArchiveTestParsingSuccessfully.class);
     final static File correctTarFiles = new File("testdata/positive/");
 
-    final File file;
 
-    @Before
+    @BeforeEach
     public void initialize() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
 
-    @Parameterized.Parameters
-    public static Collection<File> filesToTest(){
-
-        logger.debug("checking for Tars in "+correctTarFiles.getName());
-        if(!correctTarFiles.isDirectory() || correctTarFiles.listFiles() == null){
-            fail("not a directory.");
-        }
-        return Arrays.asList(correctTarFiles.listFiles());
+    public static Stream<Arguments> filesToTest(){
+        return Stream.of(
+                //FIXME: Add the rest of the files here to test
+                Arguments.of(new File("testdata/positive/4b5ba740-06fe-4506-9afc-e9f1eabadaa4.tar")));
     }
 
-
-    public LogMessageArchiveTestParsingSuccessfully(File file){
-        this.file = file;
-    }
-
-    @Ignore
-    @Test
-    public void parse() throws IOException, BadFormatForTARException {
+    @ParameterizedTest(name = "LogMessageArchiveTestParsingSuccessfully. Rest {index} => file={0}")
+    @MethodSource("filesToTest")
+    public void parse(File file) throws IOException, BadFormatForTARException {
         logger.debug("");
         logger.debug("============================================================================");
         logger.debug("testing tar file {}:", file.getName());
 
-        LogMessageArchiveImplementation tar  = new LogMessageArchiveImplementation(this.file);
-//        LogMessageReporter testReporter = new LogMessageReporter();
+        LogMessageArchiveImplementation tar  = new LogMessageArchiveImplementation(file);
 
-//        logger.debug(testReporter.createReport(Collections.singleton(tar), null).toString());
 
         Validator v = new AggregatedValidator()
                 .add(new CertificateFileNameValidator())
